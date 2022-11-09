a9ea84df0a93028110ece5f0ad4ad611bc4a0e06
67scaryboy <67scaryboy@gmail.com>
2022-11-09 12:32:41 +0100

    rename enemis

--- personnages.py
+++ personnages.py
@@ -13,7 +13,7 @@ class Enemy(pygame.sprite.Sprite):
       def __init__(self):
         super().__init__()
         self.active = 1
-        self.image = pygame.image.load("Enemy.png")
+        self.image = pygame.image.load("e1.png")
         self.rect = self.image.get_rect()
         self.rect.center=(random.randint(const.ZONE_MORTE + 50,const.SCREEN_WIDTH-50),0)
 
@@ -56,7 +56,7 @@ class Player(pygame.sprite.Sprite):
 class Compagon(pygame.sprite.Sprite):
     def __init__(self,perso):
         super().__init__()
-        self.image = pygame.image.load("Enemy.png")
+        self.image = pygame.image.load("e1.png")
         self.rect = self.image.get_rect()
         self.rect.center = perso.rect.center
         self.rect.right = perso.rect.left-10
