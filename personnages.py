abef0123967b80ab524b237bd012343574e8fd5e
67scaryboy <67scaryboy@gmail.com>
2022-11-09 11:37:34 +0100

    Ajout compagon

--- personnages.py
+++ personnages.py
@@ -2,6 +2,7 @@ import pygame, sys
 from pygame.locals import *
 import random
 import constantes as const
+import personnages
 
 
 DISPLAYSURF = pygame.display.set_mode((const.SCREEN_WIDTH,const.SCREEN_HEIGHT))
@@ -49,5 +50,37 @@ class Player(pygame.sprite.Sprite):
               if pressed_keys[K_RIGHT]:
                   self.rect.move_ip(7.5, 0)
 
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+class Compagon(pygame.sprite.Sprite):
+    def __init__(self,perso):
+        super().__init__()
+        self.image = pygame.image.load("Player.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = perso.rect.center
+
+    def update(self,perso):
+        pressed_keys = pygame.key.get_pressed()
+        if self.rect.top > 0:
+            if pressed_keys[K_UP]:
+                self.rect.move_ip(0, -7.5)
+
+        if (self.rect.bottom < const.SCREEN_HEIGHT):
+            if pressed_keys[K_DOWN]:
+                self.rect.move_ip(0,7.5)
+
+        if self.rect.left > const.ZONE_MORTE:
+              if pressed_keys[K_LEFT]:
+                  self.rect.move_ip(-7.5, 0)
+        if self.rect.right < const.SCREEN_WIDTH:
+              if pressed_keys[K_RIGHT]:
+                  self.rect.move_ip(7.5, 0)
+        deplacementX= random.randint(-5,5)
+        deplacementY= random.randint(-5,5)
+        if ((self.rect.top+deplacementY-perso.rect.top<20 or perso.rect.top-self.rect.top<20) and self.rect.bottom<const.SCREEN_HEIGHT and self.rect.bottom >0):
+            self.rect.top = self.rect.top+deplacementY
+        if ((self.rect.right+deplacementX-perso.rect.right<20 or perso.rect.right-self.rect.right<20) and self.rect.right<const.SCREEN_WIDTH and self.rect.left >0):
+            self.rect.right = self.rect.right+deplacementX
     def draw(self, surface):
         surface.blit(self.image, self.rect)
