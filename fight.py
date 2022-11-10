3e294e3022a76a23f3d10ea5309003efc6881733
Centaure111 <louiscoutard@hotmail.fr>
2022-11-10 09:31:15 +0100

    fonction arcade

--- fight.py
+++ fight.py
@@ -1,6 +1,6 @@
 import pygame, sys
 from pygame.locals import *
-import random, personnages
+import random, personnages, menu
 import constantes as const
 
 def Spawn(enemies,q):#random, pour le mode arcade
@@ -14,6 +14,74 @@ def Spawn(enemies,q):#random, pour le mode arcade
         elif p2 == 3:
             enemy  = personnages.Enemy(3)
         enemies.append(enemy)
+
+def Arcade():
+    FramePerSec = pygame.time.Clock()
+
+    AP = menu.Arrièreplan()
+    MG = menu.MenuGauche()
+    P1 = personnages.Player(1)
+    E1 = personnages.Enemy(1)
+    CP = personnages.Compagon(P1)
+
+    enemies = [] 
+    enemies.append(E1)
+    tirs = []
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        P1.update()
+        CP.update(P1)
+        for entity in enemies:
+            if entity.active == 1:
+                entity.move()
+                #ici pour décider si il tire
+                p = random.randint(0,100)
+                if p < 1:
+                    shoot = Projectile(entity)
+                    tirs.append(shoot)
+                if entity.rect.bottom > const.SCREEN_HEIGHT:
+                    enemies.remove(entity)
+    
+        if P1.cooldown == 0:
+            shoot = Projectile(P1)
+            tirs.append(shoot)
+            P1.cooldown = 60 #trouver un moyen pour récupérer la valeur initiale
+        else:
+            P1.cooldown += -1
+    
+        for shoot in tirs:
+            shoot.move()
+            if shoot.rect.bottom > const.SCREEN_HEIGHT:
+                    tirs.remove(shoot)
+    
+        AP.move()
+
+        Spawn(enemies,2)
+
+        personnages.DISPLAYSURF.fill(const.WHITE)
+        AP.draw(personnages.DISPLAYSURF)
+
+        for shoot in tirs:
+            personnages.DISPLAYSURF.blit(entity.image, entity.rect)
+            shoot.draw(personnages.DISPLAYSURF)
+
+        for entity in enemies:
+            if entity.active == 1:
+                personnages.DISPLAYSURF.blit(entity.image, entity.rect)
+                entity.draw(personnages.DISPLAYSURF)
+        P1.draw(personnages.DISPLAYSURF)
+        CP.draw(personnages.DISPLAYSURF)
+        MG.draw(personnages.DISPLAYSURF)
+
+
+        pygame.display.update()
+        FramePerSec.tick(const.FPS)
+
 
 class Projectile(pygame.sprite.Sprite):
       def __init__(self, tireur):
