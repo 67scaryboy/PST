c41e9357e4920b22e6cdff543a7be3d1b8f9ad08
Centaure111 <louiscoutard@hotmail.fr>
2022-11-21 12:58:01 +0100

    préparation bonus

--- bonus.py
+++ bonus.py
@@ -4,9 +4,10 @@ import random, personnages, menu, fight
 import constantes as const
 
 class boost_ATK():
-    def __init__(self,origine,value):
+    def __init__(self,origine,value,catégorie):
         super().__init__()
         self.boost = value
+        self.type = catégorie
         self.image = pygame.image.load("sprites/tir.png")#modifier l'image
         self.rect = self.image.get_rect()
         self.rect.center = origine.rect.center
@@ -22,3 +23,6 @@ class boost_ATK():
     
     def boost(self,p_P1):
         p_P1.ATK += self.boost
+    
+    def heal(self,p_P1):
+        p_P1.PV += self.boost
