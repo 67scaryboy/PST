025d195d946bf2ebe2e8f6943a5e361a0b2f8e30
67scaryboy <67scaryboy@gmail.com>
2022-12-12 09:02:14 +0100

    Création template fichier mission

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame, sys
-import mission1
+import mission1, mission2, mission3, mission4, mission5, mission6, mission7, mission8, mission9, mission10 
 from pygame.locals import *
 import random, personnages, menu, fight, bonus, boss
 import constantes as const
@@ -22,4 +22,22 @@ while True:
         choix = menu.MenuHistoire(const.Niveau) #Retourne le numero du niveau souhaité (1,2,...,9,10)
         if choix == 1:
             mission1.LancerMission1()
+        elif choix== 2:
+            mission2.LancerMission2()
+        elif choix== 3:
+            mission3.LancerMission3()
+        elif choix== 4:
+            mission4.LancerMission4()
+        elif choix== 5:
+            mission5.LancerMission5()
+        elif choix== 6:
+            mission6.LancerMission6()
+        elif choix== 7:
+            mission7.LancerMission7()
+        elif choix== 8:
+            mission8.LancerMission8()
+        elif choix== 9:
+            mission9.LancerMission9()
+        elif choix== 10:
+            mission10.LancerMission10()
         #scoretotal += boss.Bossfight()
---
+++ mission4.py
@@ -0,0 +1,88 @@
+import pygame, sys, math, fight
+from pygame.locals import *
+import personnages, menu, bonus
+import constantes as const
+
+def LancerMission4():
+    FramePerSec = pygame.time.Clock()
+    scoreArcade = 0
+    alive = True
+
+    AP = menu.Arrièreplan(3)# 1 a 3 pour le fond
+    AP2= menu.Arrièreplan(5)# 4 ou 5 pour le paralax profond
+    AP3= menu.Arrièreplan(6)# 6 ou 7 pour le paralax superieur
+    MB = menu.Affichage("sprites/mb.png",const.SCREEN_WIDTH/2,const.SCREEN_HEIGHT+130)
+    P1 = personnages.Player(menu.ChoixPerso())
+    CP = personnages.Compagon(P1)
+    pygame.mouse.set_pos(const.SCREEN_WIDTH//2,const.SCREEN_HEIGHT-200)
+
+    cooldown = P1.cooldown
+
+    enemies = [] 
+    tirs = []
+    explo = []
+    boosts = []
+
+    while alive:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        #tir automatique
+        if P1.cooldown == 0:
+            shoot = fight.Projectile(P1,0)
+            shootf= fight.Projectile(CP,0)
+            tirs.append(shoot)
+            tirs.append(shootf)
+            P1.cooldown = cooldown
+        else:
+            P1.cooldown += -1
+
+        #faire avance les tirs
+        for shoot in tirs:
+            shoot.move()
+            if shoot.rect.bottom > const.SCREEN_HEIGHT:
+                    tirs.remove(shoot)
+
+        ###Partie graphique###
+        personnages.DISPLAYSURF.fill(const.WHITE)
+
+        #Déplacement et affichage des images de fond
+        AP3.move(3)#vitesse de déplacement des couches
+        AP2.move(2)
+        AP.move(1)#laisser 1 pour le fond, sinon ca file la gerbe
+        AP.draw(personnages.DISPLAYSURF)
+        AP2.draw(personnages.DISPLAYSURF)
+        AP3.draw(personnages.DISPLAYSURF)
+
+        #Affichage des tirs
+        for shoot in tirs:
+            if shoot.trajectoire == 3 and shoot.tireur_id == "e1":
+                menu.Animation(const.boules,shoot)
+            shoot.draw(personnages.DISPLAYSURF)
+        
+        #Affichage des bonus
+        for boost in boosts:
+            boost.draw(personnages.DISPLAYSURF)
+
+        #Affichage des explosions
+        menu.aff_explo(explo)
+        for boom in explo:
+            menu.Animation(const.explosions,boom)
+
+        #Affichage des adversaires restants
+        for entity in enemies:
+            if entity.active == 1:
+                entity.draw(personnages.DISPLAYSURF)
+        P1.souris(personnages.DISPLAYSURF)#Affichage joueur
+        P1.draw_health(personnages.DISPLAYSURF)
+        CP.update(P1)
+        CP.draw(personnages.DISPLAYSURF)#Affichage Compagnon
+        MB.draw(personnages.DISPLAYSURF)#Affichage menu bas
+        menu.AfficheScore(scoreArcade) #Affichage score
+
+
+        pygame.display.update()
+        FramePerSec.tick(const.FPS)
+        
