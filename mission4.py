a03631854dc97da53cfbf591713c62769e906b6b
67scaryboy <67scaryboy@gmail.com>
2023-01-06 23:00:43 +0100

    Debut préparation lvl 4

--- mission4.py
+++ mission4.py
@@ -1,43 +1,355 @@
-import pygame, sys, math, fight
+import pygame, sys, math, fight, time, cinematiques, pickle, random, boss
 from pygame.locals import *
-import personnages, menu, bonus
-import constantes as const
+import personnages, menu, bonus, gc
+import constantes as const    
 
 def LancerMission4():
-    FramePerSec = pygame.time.Clock()
-    scoreArcade = 0
-    alive = True
 
-    AP = menu.Arrièreplan(3)# 1 a 3 pour le fond
+    FramePerSec = pygame.time.Clock()
+    score = 0
+    alive = True
+    dialogue = 1
+    numformation=0
+    appui = False
+    VaisseauChoisis = menu.ChoixPerso()
+    AP = menu.Arrièreplan(2)# 1 a 3 pour le fond
     AP2= menu.Arrièreplan(5)# 4 ou 5 pour le paralax profond
     AP3= menu.Arrièreplan(6)# 6 ou 7 pour le paralax superieur
     MB = menu.Affichage("sprites/mb.png",const.SCREEN_WIDTH/2,const.SCREEN_HEIGHT+130)
-    P1 = personnages.Player(menu.ChoixPerso())
+    P1 = personnages.Player(VaisseauChoisis)
     CP = personnages.Compagon(P1)
+    bulle = menu.Affichage("sprites/bulletexte.png",const.SCREEN_WIDTH-270,110)
     pygame.mouse.set_pos(const.SCREEN_WIDTH//2,const.SCREEN_HEIGHT-200)
-
     cooldown = P1.cooldown
 
     enemies = [] 
     tirs = []
     explo = []
     boosts = []
-
+    gc.collect()
     while alive:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-        
+        if pygame.mouse.get_pressed()[0] and not appui: #Pour faire en sorte qui si on laisse le clic envoncé, ca ne skip pas le dialogue
+            dialogue+=1
+            appui = pygame.mouse.get_pressed()[0]
+        if not pygame.mouse.get_pressed()[0]:
+            appui = pygame.mouse.get_pressed()[0]
+        personnages.DISPLAYSURF.fill(const.WHITE)
+        AP3.move(3)#vitesse de déplacement des couches
+        AP2.move(2)
+        AP.move(1)#laisser 1 pour le fond, sinon ca file la gerbe
+        AP.draw(personnages.DISPLAYSURF)
+        AP2.draw(personnages.DISPLAYSURF)
+        AP3.draw(personnages.DISPLAYSURF)
+        P1.draw(personnages.DISPLAYSURF)
+        bulle.draw(personnages.DISPLAYSURF)
+        if dialogue==1:
+            font = pygame.font.SysFont("arial", 13)
+            texte=font.render("Enfin on a réussit à rétablir les communications !", True, const.BLACK)
+            texterect=texte.get_rect()
+            texterect.center=(465,85)
+            personnages.DISPLAYSURF.blit(texte,texterect)
+            texte=font.render("...", True, const.BLACK)
+            texterect=texte.get_rect()
+            texterect.center=(465,100)
+            personnages.DISPLAYSURF.blit(texte,texterect)
+            texte=font.render("Je ne me souvenais pas que votre tête était aussi ...", True, const.BLACK)
+            texterect=texte.get_rect()
+            texterect.center=(465,115)
+            personnages.DISPLAYSURF.blit(texte,texterect)
+            texte=font.render("Bref", True, const.BLACK)
+            texterect=texte.get_rect()
+            texterect.center=(465,130)
+            personnages.DISPLAYSURF.blit(texte,texterect)
+        elif dialogue==2:
+            font = pygame.font.SysFont("arial", 13)
+            texte=font.render("J'ai une mauvaise nouvelle soldat.", True, const.BLACK)
+            texterect=texte.get_rect()
+            texterect.center=(465,85)
+            personnages.DISPLAYSURF.blit(texte,texterect)
+            texte=font.render("Nos radars longue portée ont détctée un énorme vaisseau alien", True, const.BLACK)
+            texterect=texte.get_rect()
+            texterect.center=(465,100)
+            personnages.DISPLAYSURF.blit(texte,texterect)
+            texte=font.render("tout proche de vous. Vous allez surement l'avoir bientot en", True, const.BLACK)
+            texterect=texte.get_rect()
+            texterect.center=(465,115)
+            personnages.DISPLAYSURF.blit(texte,texterect)
+            texte=font.render("visuel.", True, const.BLACK)
+            texterect=texte.get_rect()
+            texterect.center=(465,130)
+            personnages.DISPLAYSURF.blit(texte,texterect)
+        elif dialogue==3:
+            font = pygame.font.SysFont("arial", 13)
+            texte=font.render("Nouveaux ordres prioritaires:", True, const.BLACK)
+            texterect=texte.get_rect()
+            texterect.center=(465,85)
+            personnages.DISPLAYSURF.blit(texte,texterect)
+            texte=font.render("Abatez cette menace à tout prix", True, const.BLACK)
+            texterect=texte.get_rect()
+            texterect.center=(465,100)
+            personnages.DISPLAYSURF.blit(texte,texterect)
+            texte=font.render("Selon nos informations, ce truc transporte des vaisseaux", True, const.BLACK)
+            texterect=texte.get_rect()
+            texterect.center=(465,115)
+            personnages.DISPLAYSURF.blit(texte,texterect)
+            texte=font.render("aliens. Si vous arrivez à l'abattre, il est possible que vous", True, const.BLACK)
+            texterect=texte.get_rect()
+            texterect.center=(465,130)
+            personnages.DISPLAYSURF.blit(texte,texterect)
+            texte=font.render("arriviez à en ramener un avec vous.", True, const.BLACK)
+            texterect=texte.get_rect()
+            texterect.center=(465,145)
+            personnages.DISPLAYSURF.blit(texte,texterect)
+        elif dialogue==4:
+            font = pygame.font.SysFont("arial", 13)
+            texte=font.render("On vous recontactera dès qu'on ne verra plus ce gros truc sur", True, const.BLACK)
+            texterect=texte.get_rect()
+            texterect.center=(465,85)
+            personnages.DISPLAYSURF.blit(texte,texterect)
+            texte=font.render("les écrans radar.", True, const.BLACK)
+            texterect=texte.get_rect()
+            texterect.center=(465,100)
+            personnages.DISPLAYSURF.blit(texte,texterect)
+            texte=font.render("", True, const.BLACK)
+            texterect=texte.get_rect()
+            texterect.center=(465,115)
+            personnages.DISPLAYSURF.blit(texte,texterect)
+            texte=font.render("Bonne chance", True, const.BLACK)
+            texterect=texte.get_rect()
+            texterect.center=(465,130)
+            personnages.DISPLAYSURF.blit(texte,texterect)
+            texte=font.render("Vous allez en avoir besoin", True, const.BLACK)
+            texterect=texte.get_rect()
+            texterect.center=(465,145)
+            personnages.DISPLAYSURF.blit(texte,texterect)
+        else:
+            tempsdemarrage = time.time() #A mettre ici, sinon les adversaires risquent de spawn pendant le dialogue.
+            break
+                
+        pygame.display.update()
+        FramePerSec.tick(const.FPS)
+    pygame.mouse.set_pos(const.SCREEN_WIDTH//2,const.SCREEN_HEIGHT-200)
+    while alive:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        score,alive=fight.Colision(tirs,P1,enemies,explo,boosts,score,alive)
+        bonus.AttraperBoost(boosts,P1)
+
+        for boost in boosts:
+                    boost.move()
+                    if boost.rect.bottom > const.SCREEN_HEIGHT:
+                            boosts.remove(boost)
+
+        #Boucle de spawn après timer
+        tempspasse = time.time() - tempsdemarrage
+        if tempspasse > 5 and numformation==0: # Temps en secondes
+            ###
+            numformation=1
+        elif tempspasse > 10 and numformation==1: 
+            ###
+            numformation=2
+        elif tempspasse > 11 and numformation==2:
+            ###
+            numformation=3
+        elif tempspasse > 12 and numformation==3:
+            ###
+            numformation=4
+        elif tempspasse > 13 and numformation==4:
+            ###
+            numformation=5
+        elif tempspasse > 14 and numformation==5:
+            ###
+            numformation=6
+        elif tempspasse > 15 and numformation==6:
+            ###
+            numformation=7
+        elif tempspasse > 16 and numformation==7:
+            ###
+            numformation=8
+        elif tempspasse > 17 and numformation==8:
+            ###
+            numformation=9
+        elif tempspasse > 18 and numformation==9:
+            ###
+            numformation=10
+        elif tempspasse > 19 and numformation==10:
+            #
+            numformation=11
+        elif tempspasse > 22 and numformation==11:
+            #
+            numformation=12
+        elif tempspasse > 25 and numformation==12:
+            #
+            numformation=13
+        elif tempspasse > 30 and numformation==13:
+            #
+            numformation=14
+        elif tempspasse > 35 and numformation==14:
+            #
+            numformation+=1
+        elif tempspasse > 36 and numformation==15:
+            #
+            numformation+=1
+        elif tempspasse > 37 and numformation==16:
+            #
+            numformation+=1
+        elif tempspasse > 38 and numformation==17:
+            #
+            numformation+=1
+        elif tempspasse > 39 and numformation==18:
+            #
+            numformation+=1
+        elif tempspasse > 40 and numformation==19:
+            #
+            numformation+=1
+        elif tempspasse > 41 and numformation==20:
+            #
+            numformation+=1
+        elif tempspasse > 42 and numformation==21:
+            #
+            numformation+=1
+        elif tempspasse > 43 and numformation==22:
+            #
+            numformation+=1
+        elif len(enemies) and numformation==23: #Début du combat de boss
+            score = boss.temp(P1,score,AP3,AP2,AP,VaisseauChoisis)
+            numformation+=1
+        elif numformation==24 and len(enemies)==0:
+            with open('sauvegarde.pkl', 'rb') as f:
+                temp = pickle.load(f)
+            if temp['Histoire']==1:
+                temp['Histoire']=2
+            with open('sauvegarde.pkl', 'wb') as f:
+                    pickle.dump(temp, f)
+            menu.MenuFinPartie(score,True)
+            break
+
+
         #tir automatique
         if P1.cooldown == 0:
-            shoot = fight.Projectile(P1,0,"sprites/tira.png")
-            shootf= fight.Projectile(CP,0,"sprites/tira.png")
+            with open('sauvegarde.pkl', 'rb') as f: #Chargement de la sauvegarde pour voir si on à débloqué ou pas les vaisseaux
+                temp = pickle.load(f)
+            if VaisseauChoisis==1: #Permet de changer le sprite des tirs en fonction du nombre d'amélioration d'attaque
+                if temp['V1'][4]==0:
+                    shoot = fight.Projectile(P1,0,"sprites/tira.png")
+                    shootf= fight.Projectile(CP,0,"sprites/tira.png")
+                elif temp['V1'][4]==1:
+                    shoot = fight.Projectile(P1,0,"sprites/tira2.png")
+                    shootf= fight.Projectile(CP,0,"sprites/tira2.png")
+                elif temp['V1'][4]==2:
+                    shoot = fight.Projectile(P1,0,"sprites/tira3.png")
+                    shootf= fight.Projectile(CP,0,"sprites/tira3.png")
+            elif VaisseauChoisis==2: 
+                if temp['V2'][4]==0:
+                    shoot = fight.Projectile(P1,0,"sprites/tira2.png")
+                    shootf= fight.Projectile(CP,0,"sprites/tira2.png")
+                elif temp['V2'][4]==1:
+                    shoot = fight.Projectile(P1,0,"sprites/tira2.png")
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(P1,0,"sprites/tira2.png")
+                    shoot.rect.right=P1.rect.right
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(P1,0,"sprites/tira2.png")
+                    shoot.rect.left=P1.rect.left
+                    shootf= fight.Projectile(CP,0,"sprites/tira2.png")
+                elif temp['V2'][4]==2:
+                    shoot = fight.Projectile(P1,0,"sprites/tira2.png")
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(P1,0,"sprites/tira2.png")
+                    shoot.rect.right=P1.rect.right
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(P1,0,"sprites/tira2.png")
+                    shoot.rect.left=P1.rect.left
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(P1,6,"sprites/tira2.png")
+                    shoot.rect.right=P1.rect.right
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(P1,7,"sprites/tira2.png")
+                    shoot.rect.left=P1.rect.left
+                    shootf= fight.Projectile(CP,0,"sprites/tira2.png")
+            elif VaisseauChoisis==3:
+                if temp['V3'][4]==0:
+                    shoot = fight.Projectile(P1,0,"sprites/tira2.png")
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(P1,0,"sprites/tira2.png")
+                    shoot.rect.right=P1.rect.right
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(P1,0,"sprites/tira2.png")
+                    shoot.rect.left=P1.rect.left
+                    shootf= fight.Projectile(CP,0,"sprites/tira2.png")
+                elif temp['V3'][4]==1:
+                    shoot = fight.Projectile(P1,0,"sprites/tira2.png")
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(P1,0,"sprites/tira2.png")
+                    shoot.rect.right=P1.rect.right
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(P1,0,"sprites/tira2.png")
+                    shoot.rect.left=P1.rect.left
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(P1,6,"sprites/tira2.png")
+                    shoot.rect.right=P1.rect.right
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(P1,7,"sprites/tira2.png")
+                    shoot.rect.left=P1.rect.left
+                    shootf= fight.Projectile(CP,0,"sprites/tira2.png")
+                elif temp['V3'][4]==2:
+                    shoot = fight.Projectile(P1,0,"sprites/tira2.png")
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(P1,0,"sprites/tira2.png")
+                    shoot.rect.right=P1.rect.right
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(P1,0,"sprites/tira2.png")
+                    shoot.rect.left=P1.rect.left
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(P1,6,"sprites/tira2.png")
+                    shoot.rect.right=P1.rect.right
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(P1,7,"sprites/tira2.png")
+                    shoot.rect.left=P1.rect.left
+                    shootf= fight.Projectile(CP,0,"sprites/tira2.png")
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(P1,8,"sprites/tira2.png")
+                    shoot.rect.left=P1.rect.left
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(P1,9,"sprites/tira2.png")
+                    shoot.rect.left=P1.rect.right
             tirs.append(shoot)
             tirs.append(shootf)
             P1.cooldown = cooldown
         else:
             P1.cooldown += -1
+        
+        for entity in enemies: #Déplacement linéaire des ennemis !A CHANGER!
+            if entity.active == 1:
+                entity.move()
+                entity.moveKamikaze(P1)
+            p = random.randint(0,300)
+            if p < 1:
+                if (entity.id == "e1"):
+                    shoot = fight.Projectile(entity,3,"sprites_animation/boule1.png")
+                elif (entity.id == "e2"):
+                    shoot = fight.Projectile(entity,2,"sprites/tir3.png")
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(entity,1,"sprites/tir3.png")
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(entity,0,"sprites/tir3.png")
+                elif (entity.id == "e3"):
+                    shoot = fight.Projectile(entity,0,"sprites/tir.png")
+                elif (entity.id == "e5"):
+                    shoot = fight.Projectile(entity,4,"sprites_animation/boule1.png")
+                    tirs.append(shoot)
+                    shoot = fight.Projectile(entity,5,"sprites_animation/boule1.png")
+                tirs.append(shoot)
+            if entity.rect.bottom > const.SCREEN_HEIGHT:
+                    enemies.remove(entity)
+
 
         #faire avance les tirs
         for shoot in tirs:
@@ -80,9 +392,11 @@ def LancerMission4():
         CP.update(P1)
         CP.draw(personnages.DISPLAYSURF)#Affichage Compagnon
         MB.draw(personnages.DISPLAYSURF)#Affichage menu bas
-        menu.AfficheScore(scoreArcade) #Affichage score
+        menu.AfficheScore(score) #Affichage score
 
 
         pygame.display.update()
         FramePerSec.tick(const.FPS)
+    if alive != True: #En cas de victoire, on sort de la boucle avec alive=True
+        menu.MenuFinPartie(score,False)#Dans le menu, le score est ajouté comme argent
         
